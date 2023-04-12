484e891599e81f5aa5328db631e43126f92852bb
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-04-12 16:58:08 +0900

    feat : initial setting

---
+++ src/test/java/communication/springchatgptcommunication/SpringChatgptCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package communication.springchatgptcommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatgptCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
