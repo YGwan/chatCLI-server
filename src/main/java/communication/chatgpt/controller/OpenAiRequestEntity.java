8b69cfd744413fc56a2e3192601eddada34e45c0
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-05-03 17:51:02 +0900

    feat : 요청 DTO 파싱 후 HttpEntity 생성

--- src/main/java/communication/chatgpt/controller/OpenAiRequestEntity.java
+++ src/main/java/communication/chatgpt/controller/OpenAiRequestEntity.java
@@ -2,7 +2,11 @@ package communication.chatgpt.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import communication.chatgpt.data.Chat;
 import communication.chatgpt.data.Edits;
+import communication.chatgpt.dto.chat.request.ChatParsedRequestDto;
+import communication.chatgpt.dto.chat.request.ChatRequestDto;
+import communication.chatgpt.dto.chat.response.ChatMessageDto;
 import communication.chatgpt.dto.edits.request.EditsParsedRequestDto;
 import communication.chatgpt.dto.edits.request.EditsRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +14,27 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+
 @RequiredArgsConstructor
 @Component
 public class OpenAiRequestEntity {
 
     private final ObjectMapper objectMapper;
     private final HttpHeaders headers;
+
+    public HttpEntity<String> chatParsed(ChatRequestDto chatRequest) throws JsonProcessingException {
+
+        ChatMessageDto chatMessageDto = new ChatMessageDto(Chat.ROLE.data(), chatRequest.getContent());
+
+        String chatOpenAiBody = objectMapper.
+                writeValueAsString(
+                        new ChatParsedRequestDto(
+                                Chat.MODEL.data(),
+                                Collections.singletonList(chatMessageDto)
+                        ));
+        return new HttpEntity<>(chatOpenAiBody, headers);
+    }
 
     public HttpEntity<String> editsParsed(EditsRequestDto editRequest) throws JsonProcessingException {
         String editsOpenAiBody = objectMapper.
