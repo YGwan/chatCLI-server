56bcc97e43e2148cc16ba08829ba38bf1dd74d25
good0619@naver.com <32181771@dankook.ac.kr>
2023-05-06 00:10:54 +0900

    feat : requestBody 형식 변경(String으로 요청을 받게끔 변경)

--- src/main/java/communication/chatgpt/controller/OpenAiController.java
+++ src/main/java/communication/chatgpt/controller/OpenAiController.java
@@ -1,8 +1,6 @@
 package communication.chatgpt.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import communication.chatgpt.dto.chat.request.ChatRequestDto;
-import communication.chatgpt.dto.completions.request.CompletionsRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
@@ -20,31 +18,31 @@ public class OpenAiController {
     private final OpenAiRequestEntity openAiRequestEntity;
 
     @PostMapping("/chat/completions")
-    public ResponseEntity<String> chat(@RequestBody ChatRequestDto request) throws JsonProcessingException {
+    public ResponseEntity<String> chat(@RequestBody String request) throws JsonProcessingException {
         HttpEntity<String> openAiRequest = openAiRequestEntity.chatParsed(request);
         return openAiResponseEntity.chatParsed(openAiRequest);
     }
 
     @PostMapping("/gc")
-    public ResponseEntity<String> grammarCheck(@RequestBody CompletionsRequestDto request) throws JsonProcessingException {
+    public ResponseEntity<String> grammarCheck(@RequestBody String request) throws JsonProcessingException {
         HttpEntity<String> openAiRequest = openAiRequestEntity.grammarCheckParsed(request);
         return openAiResponseEntity.completionsParsed(openAiRequest);
     }
 
     @PostMapping("/mood")
-    public ResponseEntity<String> tweetClassifier(@RequestBody CompletionsRequestDto request) throws JsonProcessingException {
+    public ResponseEntity<String> tweetClassifier(@RequestBody String request) throws JsonProcessingException {
         HttpEntity<String> openAiRequest = openAiRequestEntity.tweetClassifierParsed(request);
         return openAiResponseEntity.completionsParsed(openAiRequest);
     }
 
     @PostMapping("/trans")
-    public ResponseEntity<String> translate(@RequestBody CompletionsRequestDto request) throws JsonProcessingException {
+    public ResponseEntity<String> translate(@RequestBody String request) throws JsonProcessingException {
         HttpEntity<String> openAiRequest = openAiRequestEntity.translateParsed(request);
         return openAiResponseEntity.completionsParsed(openAiRequest);
     }
 
     @PostMapping("/summarize")
-    public HttpEntity<String> summarize(@RequestBody CompletionsRequestDto request) throws JsonProcessingException {
+    public HttpEntity<String> summarize(@RequestBody String request) throws JsonProcessingException {
         HttpEntity<String> openAiRequest = openAiRequestEntity.summarizeParsed(request);
         return openAiResponseEntity.summarizeParsed(openAiRequest);
     }
