b87feb5e7e8441890d90627327ed35ac32c72441
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-05-09 00:59:50 +0900

    feat : chat api 테스트 코드 작성

--- src/test/java/communication/chatgpt/controller/ChatControllerTest.java
+++ src/test/java/communication/chatgpt/controller/ChatControllerTest.java
@@ -1,36 +1,65 @@
 package communication.chatgpt.controller;
 
-import communication.chatgpt.dto.edits.request.EditsRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import communication.chatgpt.data.Chat;
+import communication.chatgpt.dto.chat.request.ChatParsedRequestDto;
+import communication.chatgpt.dto.chat.response.ChatMessageDto;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ActiveProfiles("default")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChatControllerTest {
+    @Value("${open-ai.token}")
+    private static String token;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private TestRestTemplate rt;
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
 
-    @DisplayName("EDITS 명령어 테스트")
+    @BeforeAll
+    public static void init(){
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+    }
+
     @Test
-    void EDITS_명령어_테스트() {
-        final String input = "What day of the wek is it?";
-        final EditsRequestDto request = new EditsRequestDto(input);
+    @DisplayName("chat api 테스트")
+    public void chatApi_Test() throws Exception {
+        // given
+        ChatParsedRequestDto chatParsedRequestDto = new ChatParsedRequestDto();
+        ChatMessageDto chatMessageDto = new ChatMessageDto(Chat.ROLE.data(), "Hello");
+        chatParsedRequestDto.setModel(Chat.MODEL.data());
+        chatParsedRequestDto.setMessages(Collections.singletonList(chatMessageDto));
+        String body = om.writeValueAsString(chatParsedRequestDto);
 
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity("/v1/edits", request, String.class);
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/v1/chat/completions", HttpMethod.POST, request, String.class);
+        HttpStatus statusCode = response.getStatusCode();
 
-        HttpStatus statusCode = responseEntity.getStatusCode();
-        String responseBody = responseEntity.getBody();
-
-        final String actual = "What day of the week is it?";
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String answer = dc.read("$.messages[0].content");
 
         assertEquals(HttpStatus.OK, statusCode);
-        assertEquals(actual, responseBody);
+        assertNotNull(answer);
     }
 }
