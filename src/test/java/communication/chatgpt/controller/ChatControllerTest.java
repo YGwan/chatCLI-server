8a873f437d634140099bb08a2eb00349687c9bd3
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-05-09 01:10:46 +0900

    feat : grammer check api 테스트 코드 작성

--- src/test/java/communication/chatgpt/controller/ChatControllerTest.java
+++ src/test/java/communication/chatgpt/controller/ChatControllerTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import communication.chatgpt.data.Chat;
+import communication.chatgpt.data.Completions;
 import communication.chatgpt.dto.chat.request.ChatParsedRequestDto;
 import communication.chatgpt.dto.chat.response.ChatMessageDto;
+import communication.chatgpt.dto.completions.request.CompletionsParsedRequestDto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,6 +60,28 @@ class ChatControllerTest {
         // then
         DocumentContext dc = JsonPath.parse(response.getBody());
         String answer = dc.read("$.messages[0].content");
+
+        assertEquals(HttpStatus.OK, statusCode);
+        assertNotNull(answer);
+    }
+
+    @Test
+    @DisplayName("grammar Check 테스트")
+    public void grammarCheck_Test() throws Exception {
+        // given
+        String prompt = "What day of the wek is it?";
+        CompletionsParsedRequestDto completionsParsedRequestDto = new CompletionsParsedRequestDto(Completions.MODEL.data(),prompt,prompt.length());
+        String body = om.writeValueAsString(completionsParsedRequestDto);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/v1/gc", HttpMethod.POST, request, String.class);
+        HttpStatus statusCode = response.getStatusCode();
+        System.out.println(response);
+
+        // then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String answer = dc.read("$.prompt");
 
         assertEquals(HttpStatus.OK, statusCode);
         assertNotNull(answer);
