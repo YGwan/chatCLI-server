6e682a9cab34e8bb4589a5bbc97a9c660ff4da1e
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-05-09 16:13:38 +0900

    test : translate api 테스트

--- src/test/java/communication/chatgpt/controller/OpenAiControllerTest.java
+++ src/test/java/communication/chatgpt/controller/OpenAiControllerTest.java
@@ -74,4 +74,19 @@ class OpenAiControllerTest {
         assertEquals(HttpStatus.OK, statusCode);
         assertNotNull(answer);
     }
+
+    @Test
+    @DisplayName("translate api 테스트")
+    public void translate_Test() {
+        Request += question;
+        HttpEntity<String> terminalRequest = new HttpEntity<>(Request, headers);
+        ResponseEntity<String> response = rt.exchange("/v1/trans", HttpMethod.POST, terminalRequest, String.class);
+        HttpStatus statusCode = response.getStatusCode();
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String answer = dc.read("$");
+
+        assertEquals(HttpStatus.OK, statusCode);
+        assertNotNull(answer);
+    }
 }
