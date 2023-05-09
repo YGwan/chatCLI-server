ae0edb47da6c9cbf525b4f22d6e5564239c0a18a
서지현 <79920930+ji-hyeon97@users.noreply.github.com>
2023-05-09 15:59:51 +0900

    test : chat api 테스트

---
+++ src/test/java/communication/chatgpt/controller/OpenAiControllerTest.java
@@ -0,0 +1,46 @@
+package communication.chatgpt.controller;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("default")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OpenAiControllerTest {
+
+    @Autowired
+    private TestRestTemplate rt;
+
+    private final String question = "Hello !";
+    private String chatRequest = "";
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init() {
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+    }
+
+    @Test
+    @DisplayName("chat api 테스트")
+    public void chatApi_Test() {
+        chatRequest += question;
+        HttpEntity<String> terminalRequest = new HttpEntity<>(chatRequest, headers);
+        ResponseEntity<String> response = rt.exchange("/v1/chat/completions", HttpMethod.POST, terminalRequest, String.class);
+        HttpStatus statusCode = response.getStatusCode();
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String answer = dc.read("$");
+
+        assertEquals(HttpStatus.OK, statusCode);
+        assertNotNull(answer);
+    }
+}
