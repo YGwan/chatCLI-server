0a93eec405832ed9afa5b0b99b6ad331166733d8
good0619@naver.com <32181771@dankook.ac.kr>
2023-05-24 16:27:12 +0900

    refactor : Application명 변경

--- src/main/java/communication/chatgpt/OpenAiApplication.java
+++ src/main/java/communication/chatgpt/OpenAiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatgptApplication {
+public class OpenAiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatgptApplication.class, args);
+        SpringApplication.run(OpenAiApplication.class, args);
     }
 }
