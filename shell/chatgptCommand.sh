87263e2e66a8853fc0532be9435a6a058eaa0f35
good0619@naver.com <32181771@dankook.ac.kr>
2023-05-30 18:14:10 +0900

    feat : shell script 구현

---
+++ shell/chatgptCommand.sh
@@ -0,0 +1,72 @@
+function usage() {
+  echo
+  echo "############################## chatgpt user's guide ##############################"
+  echo
+  echo "We can use a total of 9 commands served by Open ai."
+  echo
+  echo "Question ----> chatAsk -m \"question\""
+  echo "Remove previous question information(clear Session) ----> chatAsk -clear"
+  echo "Grammar check ----> chatAsk -gc \"sentence\""
+  echo "Convert a voice file to a text file ----> chatAsk -at filename.m4"
+  echo "Sentence Emotion Analysis(Positive, Negative, Neutral) ----> chatAsk -md \"sentence\""
+  echo "Translate English into Korean ----> chatAsk -t \"english sentence\""
+  echo "Sentence Summary ----> chatAsk -s \"sentence\""
+  echo "User Lookup Top 5 Keywords Lookup ----> chatAsk -rank"
+  echo
+  echo "if you want a another service, send devygwan@gmail.com"
+  echo
+  echo "#################################################################################"
+  echo
+}
+
+function chat_command() { 
+  local SERVER_IP="localhost" 
+  local SERVER_PORT="8081" 
+  local REQUEST_PATH="$1" 
+  local REQUEST_BODY="$2" 
+ 
+  if [[ "$1" == "-help" || "$1" == "-m" || "$1" == "-md" || "$1" == "-gc" || "$1" == "-at" || "$1" == "-clear" || "$1" == "-t" || "$1" == "-s" || "$1" == "-rank" ]]; then 
+    OPTION="$1" 
+    shift 
+  else 
+    echo "Unknown option: $1" 
+    echo "chatAsk instruction ---> chatAsk -help"
+    return 1 
+  fi 
+  
+  if [[ "$OPTION" == "-help" ]]; then 
+    usage 
+    return 1 
+ 
+  elif [[ "$OPTION" == "-m" ]]; then 
+    REQUEST_PATH="/v1/chat" 
+  elif [[ "$OPTION" == "-md" ]]; then 
+    REQUEST_PATH="/v1/mood" 
+  elif [[ "$OPTION" == "-gc" ]]; then 
+    REQUEST_PATH="/v1/gc" 
+  elif [[ "$OPTION" == "-at" ]]; then 
+    REQUEST_PATH="/v1/audio/transcriptions" 
+  elif [[ "$OPTION" == "-clear" ]]; then 
+    REQUEST_PATH="/v1/chat/reset" 
+  elif [[ "$OPTION" == "-t" ]]; then 
+    REQUEST_PATH="/v1/trans" 
+  elif [[ "$OPTION" == "-s" ]]; then 
+    REQUEST_PATH="/v1/summarize" 
+  elif [[ "$OPTION" == "-rank" ]]; then 
+    REQUEST_PATH="/v1/keywords" 
+  fi 
+ 
+  if [[ "$OPTION" == "-at" ]]; then 
+    curl -X POST -H "Content-Type: multipart/form-data" -F file="${REQUEST_BODY}" "http://${SERVER_IP}:${SERVER_PORT}${REQUEST_PATH}" -w '\n' 
+   
+  elif [[ "$OPTION" == "-rank" ]]; then 
+    curl -X GET "http://${SERVER_IP}:${SERVER_PORT}${REQUEST_PATH}" -w '\n' 
+   
+  else 
+    curl -X POST -H "Content-Type: text/plain" -d "$REQUEST_BODY" "http://${SERVER_IP}:${SERVER_PORT}${REQUEST_PATH}" -w '\n' 
+  fi 
+} 
+ 
+# alias 설정
+alias chatAsk='chat_command'
+
